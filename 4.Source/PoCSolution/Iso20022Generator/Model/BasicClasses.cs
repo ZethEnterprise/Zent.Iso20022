88c4d98b7630efea4f8be9d084b003858d91a951
Zeth <zethenterprise@gmail.com>
2024-04-12 15:49:17 +0200

    Adding additional files.

---
+++ 4.Source/PoCSolution/Iso20022Generator/Model/BasicClasses.cs
@@ -0,0 +1,76 @@
+﻿using System.Xml.Linq;
+using System.Xml;
+
+namespace Iso20022Generator.Model;
+public enum PropertyType { Simple, Complex, Class, Multiple };
+
+public class XObject
+{
+    public string Id;
+}
+
+public abstract class PropertyObject : XObject
+{
+    public string Name;
+    public PropertyType MyKind;
+
+    public abstract string MyStringbasedKind();
+}
+
+public class SimplePropertyObject : PropertyObject
+{
+    public string MyType;
+    public string SpecifiedType;
+    public string TraceId;
+
+    public override string MyStringbasedKind() => this.SpecifiedType;
+}
+
+public class ClassObject : XObject
+{
+    public string Name;
+    public List<PropertyObject> Properties;
+}
+
+public class ClassPropertyObject : PropertyObject
+{
+    public ClassObject MyType;
+
+    public override string MyStringbasedKind() => MyType.Name;
+}
+
+public class MasterData
+{
+    public Dictionary<string, XElement> Data { get; set; }
+    public XDocument Doc { get; set; }
+    public XmlNamespaceManager Xnm { get; set; }
+    public List<ClassObject> SchemaModels { get; } = new();
+    public Dictionary<string, CodeSet> Enums { get; set; } = new();
+
+
+    public XNamespace Prefix(string prefix) => Xnm.LookupNamespace(prefix);
+}
+
+public class CodeSet
+{
+    public string TraceId { get; set; }
+    public string Name { get; set; }
+    public XElement Xelement { get; set; }
+}
+
+public class ComplexCodeSet : CodeSet
+{
+
+}
+
+public class SimpleEnumeration : CodeSet
+{
+    public IEnumerable<Code> Codes { get; set; }
+}
+public class Code
+{
+    public string Name { get; set; }
+    public string CodeName { get; set; }
+    public string Description { get; set; }
+    public XElement Xelement { get; set; }
+}
---
+++ 4.Source/PoCSolution/Iso20022Generator/Templates/ClassTemplatePartial.cs
@@ -0,0 +1,9 @@
+﻿using Iso20022Generator.Model;
+
+namespace Iso20022Generator.Templates;
+
+public partial class ClassTemplate
+{
+    public string Namespace { get; set; }
+    public ClassObject ClassObject { get; set; }
+}
