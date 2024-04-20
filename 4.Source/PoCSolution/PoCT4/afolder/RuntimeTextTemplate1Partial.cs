5c9de0bb4ac943b69312711942916bcc516715c4
Zeth <zethenterprise@gmail.com>
2024-04-21 00:07:38 +0200

    Remoted all old crap, which is no longer needed and will only fill up the space on this Repo. Me no likey!

--- 4.Source/PoCSolution/PoCT4/afolder/RuntimeTextTemplate1Partial.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PoCT4.afolder
-{
-    public partial class RuntimeTextTemplate1
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-    }
-}
